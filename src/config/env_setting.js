342f55455aec3cc3f0e23e4412ed72c3bc0ec905
Nelson Hsu <yuus271828@gmail.com>
2022-08-31 09:14:44 +0800

    update API_HOSTNAME

--- src/config/env_setting.js
+++ src/config/env_setting.js
@@ -1,2 +1,2 @@
 // BackEnd host name
-export const API_HOSTNAME = "http://springbootonlinestorebackend-env.eba-tfseztpn.ap-northeast-1.elasticbeanstalk.com";
+export const API_HOSTNAME = "http://springbootonlinestorebackend-env-1.eba-tfseztpn.ap-northeast-1.elasticbeanstalk.com";
